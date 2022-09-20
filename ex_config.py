8094e875851ed6c352bd0f392ea204cf25269a66
UNKNOWN <79321850+Jatindalal875@users.noreply.github.com>
2022-09-20 20:00:25 +0530

    Update ex_config.py

--- ex_config.py
+++ ex_config.py
@@ -1,29 +1,45 @@
-# FOR SELF HOST
-# EDIT THIS FILE AND RENAME TO config.py TO MAKE THIS BOT WORKING
-# FILL THESE VALUES ACCORDINGLY.
-
-from HellConfig.config import Config
+from sample_config import Config
 
 
 class Development(Config):
-    # get these values from my.telegram.org.
-    
-    APP_ID = 666666  # 666666 is a placeholder. Fill your 6 digit api id
-    
-    API_HASH = "eb06d4abfb49dc3eeb1aeb98ae0f581e"  # replace this with your api hash
-    
-    BOT_TOKEN = "Your value"  # Create a bot from @BotFather and paste the token here
-    
-    BOT_LIBRARY = "telethon"  # fill 'pyrogram' if you want pyrogram version of hellbot else leave it as it is.
+  # get this values from my.telegram.org. 
+  # 6 is just a placeholder. Fill your own api id & hash.
+  APP_ID = 6
+  API_HASH = "eb06d4abfb49dc3eeb1aeb98ae0f581e"
 
-    DB_URI = "Your value"  # A postgresql database url from elephantsql
+  # the name to display in your alive message.
+  # If not filled anything then default value is Mafia User.
+  ALIVE_NAME = "HELL User"
+  ABUSE = "OFF"
+  BAN_PIC = "https://telegra.ph/"
+  # create any PostgreSQL database.
+  # I recommend to use elephantsql and paste that link here
+  DB_URI = "Your value"
 
-    HELLBOT_SESSION = "Your value"  # telethon or pyrogram string according to BOT_LIBRARY
+  # After cloning the repo and installing requirements...
+  # Do python string_session.py and fill the on screen prompts.
+  # String session will be saved in your saved message of telegram.
+  # Put that string here.
+  STRING_SESSION = "Your value"
 
-    HANDLER = "."  # Custom Command Handler
+  # Create a bot in @botfather and fill the following values with bot token and username.
+  TG_BOT_TOKEN_BF_HER = "Your value" #token
+  TG_BOT_USER_NAME_BF_HER = "Your value" #username
 
-    SUDO_HANDLER = "!"  # Custom Command Handler for sudo users.
+  # Create a private group and add rose bot to it.
+  # and type /id and paste that id here.
+  # replace that -100 with that group id.
+  PRIVATE_GROUP_BOT_API_ID = -100
+          HELLBOT_LOGGER = -1000
+  # Custom Command Handler. 
+  COMMAND_HAND_LER = "."
 
+  # enter the userid of sudo users.
+  # you can add multiple ids by separating them by space.
+  # fill values in [] only.
+  SUDO_USERS = []
 
-# end of required config
-# hellbot
+  # command hanler for sudo users.
+  SUDO_COMMAND_HAND_LER = "."
+  
+  
